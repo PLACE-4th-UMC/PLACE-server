4e328221f0aca232dfe41c55b46b10c4f1f17693
Jisoo Oh <56earls@gmail.com>
2023-07-16 12:36:59 +0900

    project settings

---
+++ place/src/main/java/com/umc/place/PlaceApplication.java
@@ -0,0 +1,13 @@
+package com.umc.place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceApplication.class, args);
+	}
+
+}
