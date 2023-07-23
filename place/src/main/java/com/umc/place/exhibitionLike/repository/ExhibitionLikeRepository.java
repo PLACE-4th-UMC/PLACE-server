fba3c5affb47f2017dc176427ab72320dab8ddb8
joonghyun <joongh@ewhain.net>
2023-07-23 18:20:04 +0900

    #1 feat: exhibitionLike mvc 패턴 생성

---
+++ place/src/main/java/com/umc/place/exhibitionLike/repository/ExhibitionLikeRepository.java
@@ -0,0 +1,8 @@
+package com.umc.place.exhibitionLike.repository;
+
+
+import com.umc.place.exhibitionLike.entity.ExhibitionLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExhibitionLikeRepository extends JpaRepository<ExhibitionLike, Long> {
+}
