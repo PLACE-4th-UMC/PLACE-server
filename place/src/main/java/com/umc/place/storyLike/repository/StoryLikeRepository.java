6339fd9033bacc92ef77f45b20323539704dab4c
joonghyun <joongh@ewhain.net>
2023-07-23 18:27:59 +0900

    #1 feat: storyLike mvc 패턴 생성

---
+++ place/src/main/java/com/umc/place/storyLike/repository/StoryLikeRepository.java
@@ -0,0 +1,7 @@
+package com.umc.place.storyLike.repository;
+
+import com.umc.place.storyLike.entity.StoryLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoryLikeRepository extends JpaRepository<StoryLike, Long> {
+}
