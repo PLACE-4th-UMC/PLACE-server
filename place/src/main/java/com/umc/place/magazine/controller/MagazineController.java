9a56108c942bd258d97013a0cb4ebf318bc6080d
joonghyun <joongh@ewhain.net>
2023-07-27 13:39:35 +0900

    #1 feat: Home, Magazine 패키지 추가

---
+++ place/src/main/java/com/umc/place/magazine/controller/MagazineController.java
@@ -0,0 +1,11 @@
+package com.umc.place.magazine.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/magazine")
+public class MagazineController {
+}
---
+++ place/src/main/java/com/umc/place/magazine/service/MagazineService.java
@@ -0,0 +1,9 @@
+package com.umc.place.magazine.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MagazineService {
+}
