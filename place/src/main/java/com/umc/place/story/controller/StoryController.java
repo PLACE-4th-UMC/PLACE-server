00c92ec637bc0201216b9595978c03507ff15391
5jisoo <56earls@gmail.com>
2023-07-29 15:49:53 +0900

    #7 feat: uploadStory API

--- place/src/main/java/com/umc/place/story/controller/StoryController.java
+++ place/src/main/java/com/umc/place/story/controller/StoryController.java
@@ -3,6 +3,8 @@ package com.umc.place.story.controller;
 import com.umc.place.common.BaseException;
 import com.umc.place.common.BaseResponse;
 import com.umc.place.story.dto.StoryDetailResponseDto;
+import com.umc.place.story.dto.StoryUploadRequestDto;
+import com.umc.place.story.dto.StoryUploadResponseDto;
 import com.umc.place.story.service.StoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,16 @@ import org.springframework.web.bind.annotation.*;
 public class StoryController {
 
     private final StoryService storyService;
+
+    @PostMapping("")
+    public BaseResponse<StoryUploadResponseDto> uploadStory(@RequestBody StoryUploadRequestDto storyUploadRequestDto,
+                                                            @RequestParam Long userId) {
+        try {
+            return new BaseResponse<>(storyService.uploadStory(storyUploadRequestDto, userId));
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
 
     @GetMapping("/{storyIdx}")
     public BaseResponse<StoryDetailResponseDto> getStoryDetail(@PathVariable Long storyIdx, @RequestParam Long userId) {
--- place/src/main/java/com/umc/place/story/service/StoryService.java
+++ place/src/main/java/com/umc/place/story/service/StoryService.java
@@ -2,7 +2,13 @@ package com.umc.place.story.service;
 
 import com.umc.place.comment.dto.CommentResDto;
 import com.umc.place.common.BaseException;
+import com.umc.place.common.BaseResponse;
+import com.umc.place.common.BaseResponseStatus;
+import com.umc.place.exhibition.entity.Exhibition;
+import com.umc.place.exhibition.repository.ExhibitionRepository;
 import com.umc.place.story.dto.StoryDetailResponseDto;
+import com.umc.place.story.dto.StoryUploadRequestDto;
+import com.umc.place.story.dto.StoryUploadResponseDto;
 import com.umc.place.story.entity.Story;
 import com.umc.place.story.repository.StoryLikeRepository;
 import com.umc.place.story.repository.StoryRepository;
@@ -10,6 +16,7 @@ import com.umc.place.user.entity.User;
 import com.umc.place.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,6 +29,7 @@ public class StoryService {
 
     private final StoryRepository storyRepository;
     private final StoryLikeRepository storyLikeRepository;
+    private final ExhibitionRepository exhibitionRepository;
     private final UserRepository userRepository;
 
     public StoryDetailResponseDto getStoryDetail(Long storyIdx, Long userId) throws BaseException {
@@ -47,6 +55,31 @@ public class StoryService {
             throw e;
         } catch (Exception e) {
             throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+    @Transactional
+    public StoryUploadResponseDto uploadStory(StoryUploadRequestDto storyUploadRequestDto, Long userId) throws BaseException {
+        try {
+            Exhibition exhibition = exhibitionRepository.findById(storyUploadRequestDto.getExhibitionIdx())
+                    .orElseThrow(() -> new BaseException(INVALID_EXHIBITION_IDX));
+            User user = userRepository.findById(userId).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
+
+            Story newStory = Story.builder()
+                    .user(user)
+                    .exhibition(exhibition)
+                    .storyImg(storyUploadRequestDto.getStoryImg())
+                    .build();
+
+            Story savedStory = storyRepository.save(newStory);
+
+            return new StoryUploadResponseDto(savedStory.getStoryIdx());
+
+        } catch (BaseException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new BaseException(DATABASE_ERROR);
+
         }
     }
 }
