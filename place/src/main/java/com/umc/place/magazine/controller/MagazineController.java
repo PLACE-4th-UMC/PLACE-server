ac36e04bd9e164465cc10dbe8503adf3da9b3e5d
lsn5963 <77337977+lsn5963@users.noreply.github.com>
2023-07-29 17:46:31 +0900

    #14 feat: magazine API 구현

--- place/src/main/java/com/umc/place/magazine/controller/MagazineController.java
+++ place/src/main/java/com/umc/place/magazine/controller/MagazineController.java
@@ -1,11 +1,22 @@
 package com.umc.place.magazine.controller;
 
+import com.umc.place.common.BaseException;
+import com.umc.place.common.BaseResponse;
+import com.umc.place.magazine.dto.GetMagazineDetailRes;
+import com.umc.place.magazine.service.MagazineService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/magazine")
 public class MagazineController {
+
+    private final MagazineService magazineService;
+    @ResponseBody
+    @GetMapping("/{userIdx}")
+    public BaseResponse<GetMagazineDetailRes> getMagazineDetailRes(@PathVariable Long userIdx) throws BaseException {
+        return new BaseResponse<>(magazineService.getMagazineDetail(userIdx));
+    }
+
 }
--- place/src/main/java/com/umc/place/story/repository/StoryLikeRepository.java
+++ place/src/main/java/com/umc/place/story/repository/StoryLikeRepository.java
@@ -6,7 +6,10 @@ import com.umc.place.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StoryLikeRepository extends JpaRepository<StoryLike, Long> {
     Boolean existsByUserAndStory(User user, Story story);
+    List<StoryLike> findByUser(User user);
 }
