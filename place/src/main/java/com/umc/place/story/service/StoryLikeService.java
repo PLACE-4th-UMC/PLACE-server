f1dd30ac35882b2663610e68902ff38c4ee23671
5jisoo <56earls@gmail.com>
2023-07-29 14:47:51 +0900

    #7 fix: StoryDetailResDto

--- place/src/main/java/com/umc/place/story/service/StoryLikeService.java
+++
@@ -1,9 +0,0 @@
-package com.umc.place.story.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class StoryLikeService {
-}
