465e57914ab07afc269774d5237ec6af4769ce04
ri-naa <rina288@ewhain.net>
2023-08-02 02:28:25 +0900

    #13 feat: 카카오 소셜 로그인

--- place/src/main/java/com/umc/place/user/entity/User.java
+++ place/src/main/java/com/umc/place/user/entity/User.java
@@ -2,10 +2,13 @@ package com.umc.place.user.entity;
 
 import com.umc.place.common.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
 
 @Entity
 @Getter
@@ -21,6 +24,9 @@ public class User extends BaseEntity {
     @Column(nullable = false, length = 10)
     private String nickname;
 
+    @Column(nullable = false, length = 50)
+    private String email;
+
     @Column
     private String userImg;
 
@@ -32,8 +38,57 @@ public class User extends BaseEntity {
     private Provider provider;
 
     @Column(nullable = false)
+    private Date birthday;
+
+    @Column(nullable = false)
     private String accessToken;
 
     @Column(nullable = false)
     private String refreshToken;
+
+    @Builder
+    public User(Long userIdx, Provider provider) {
+        this.userIdx = userIdx;
+        this.provider = provider;
+    }
+
+    public void signup(String nickname, String userImg, Date birthday, String location){
+        this.nickname = nickname;
+        this.userImg = userImg;
+        this.birthday = birthday;
+        this.location = location;
+    }
+
+    public void storeSignUp(String nickname, String userImg, Provider provider) {
+        this.nickname = nickname;
+        this.userImg = userImg;
+        this.provider = provider;
+    }
+
+    //탈퇴하기
+    public void signout() {
+        this.setNickname("알 수 없음");
+        this.setProfileImg(null);
+        this.setProvider(Provider.ANONYMOUS);
+        this.setStatus("inactive");
+    }
+
+    public void setProfileImg(String userImg) {
+        this.userImg = userImg;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    public void setProvider(Provider provider){
+        this.provider = provider;
+    }
+
+    public void logout() {
+        this.setStatus("logout");
+    }
+    public void login() {
+        this.setStatus("active");
+    }
+
+
 }
