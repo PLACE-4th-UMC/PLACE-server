a51300d0aeaddcac54f3ad5b95f8ef6c5b4a02f9
ri-naa <rina288@ewhain.net>
2023-08-02 14:36:36 +0900

    #13 feat: 카카오 소셜 로그인

---
+++ place/src/main/java/com/umc/place/user/service/AuthService.java
@@ -0,0 +1,35 @@
+package com.umc.place.user.service;
+
+import com.umc.place.user.entity.Provider;
+import com.umc.place.user.entity.User;
+
+import com.umc.place.user.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+    //카카오 구글 네이버 회원 모두 관리하기 위해 userRepository 이거로 저장
+    private final UserRepository userRepository;
+    @Autowired
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public User saveUser(User member) {
+        return userRepository.save(member);
+    }
+    public User findUserByIdAndProvider(Long userIdx, Provider provider) {
+        return userRepository.findByIdAndProvider(userIdx, provider);
+    }
+
+    public Optional<User> findUserByIdAndStatus(Long userIdx, String status) {
+        return userRepository.findByUserIdxAndStatus(userIdx, status);
+    }
+
+    public boolean existsByNickname(String nickname) {
+        return userRepository.existsByNickname(nickname);
+    }
+}
