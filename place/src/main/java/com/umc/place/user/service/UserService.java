80594e8cdde3a257972a8927b37aec520b27888a
ri-naa <rina288@ewhain.net>
2023-08-04 15:34:21 +0900

    #13 fix: UserRepository 수정

--- place/src/main/java/com/umc/place/user/service/UserService.java
+++ place/src/main/java/com/umc/place/user/service/UserService.java
@@ -4,42 +4,41 @@ import com.umc.place.common.BaseException;
 import com.umc.place.user.dto.*;
 import com.umc.place.user.entity.Provider;
 import com.umc.place.user.entity.User;
+import com.umc.place.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.umc.place.common.BaseResponseStatus.*;
 
-
 @Service
 @RequiredArgsConstructor //생성자 자동 생성
 public class UserService {
 
-    private final RepositoryService repositoryService;
+    private final UserRepository userRepository;
     private final AuthService authService;
 
-    private PostUserRes signUpOrLogin(Long useridx, Provider provider) throws BaseException {
+    private PostUserRes signUpOrLogin(String identifier, Provider provider) throws BaseException {
         //Provider provider = Provider.KAKAO;
-        User user = repositoryService.findUserByIdAndProvider(useridx, provider);
-
+        User user = userRepository.findByIdentifierAndProvider(identifier, provider);
         //기존 회원이 아닐 경우 회원가입
         if (user == null)
-            user = signup(useridx,provider); //provider 카카오로 설정
+            user = signup(identifier,provider); //provider 카카오로 설정
         //탈퇴한 회원일 경우
         if (user.getStatus().equals("inactive"))
             throw new BaseException(ALREADY_WITHDRAW_USER);
         //기존 회원이면 로그인 처리
         user.login(); //user status active로 바꾸기
-        repositoryService.saveUser(user); //userRepository에 user 저장
+        userRepository.save(user); //userRepository에 user 저장
         return authService.createToken(user); //user의 access token, refresh token 반환
     }
 
     //로그인
-    public PostUserRes login(Long useridx, String provider) throws BaseException{
+    public PostUserRes login(String identifier, String provider) throws BaseException{
         try{
             if(Provider.getProviderByName(provider) == null)
                 throw new BaseException(INVALID_PROVIDER);
-            return signUpOrLogin(useridx, Provider.getProviderByName(provider)); //access token, refresh token 반환
+            return signUpOrLogin(identifier, Provider.getProviderByName(provider)); //access token, refresh token 반환
         } catch (BaseException e){
             throw e;
         } catch (Exception e){
@@ -48,12 +47,12 @@ public class UserService {
     }
 
     //첫 로그인 시 user 생성 및 저장 (가입 연도 추가하기 createdDate)
-    public User signup(Long userIdx, Provider provider) {
+    public User signup(String identifier, Provider provider) {
         User newuser = User.builder()
-                .userIdx(userIdx)
+                .identifier(identifier)
                 .provider(provider)
                 .build();
-        return repositoryService.saveUser(newuser);
+        return userRepository.save(newuser);
     }
 
     //회원 가입 후 사용자 정보 입력
@@ -61,12 +60,12 @@ public class UserService {
     public PostUserRes signup_UserInfo(Long userIdx, PostNewUserReq postNewUserReq) throws BaseException {
         try{
             //userIdx로 해당 user 찾기
-            User user = repositoryService.findUserByIdAndStatus(userIdx, "active").orElseThrow(()->new BaseException(INVALID_USER_IDX));
+            User user = userRepository.findByUserIdxAndStatus(userIdx, "active").orElseThrow(()->new BaseException(INVALID_USER_IDX));
             String accessToken = authService.createAccessToken(userIdx);
             String refreshToken = authService.createRefreshToken(userIdx);
 
             user.signup(postNewUserReq.getNickname(), postNewUserReq.getUserImg(), postNewUserReq.getBirthday(), postNewUserReq.getLocation(), postNewUserReq.getEmail());
-            repositoryService.saveUser(user); //repository에 저장
+            userRepository.save(user); //repository에 저장
 
             return new PostUserRes(accessToken, refreshToken);
         } catch (BaseException e) {
@@ -78,11 +77,16 @@ public class UserService {
 
     //닉네임 중복 확인하기
     public void checkNickname(PostNicknameReq postNicknameReq) throws BaseException {
-        boolean existence = repositoryService.existsByNickname(postNicknameReq.getNickname());
+        boolean existence = userRepository.existsByNickname(postNicknameReq.getNickname());
         if(existence) throw new BaseException(EXIST_NICKNAME);
     }
 
     //마이페이지 조회
+    public GetProfileRes getProfile() throws BaseException {
+        Long userIdx = authService.getUserIdx();
+        User user = userRepository.findByUserIdxAndStatus(userIdx, "active").orElseThrow(() -> new BaseException(INVALID_USER_IDX));
+        return new GetProfileRes(user.getUserImg(), user.getNickname(), "Hello, " + user.getNickname());
+    }
 
 
     //사용자 프로필 수정
