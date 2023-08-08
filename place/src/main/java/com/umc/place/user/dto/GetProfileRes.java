b215b18f145b9c0f839d3ca25648a144788e7693
ri-naa <rina288@ewhain.net>
2023-08-08 20:05:34 +0900

    #13 fix: 마이페이지 스토리 추가

--- place/src/main/java/com/umc/place/user/dto/GetProfileRes.java
+++ place/src/main/java/com/umc/place/user/dto/GetProfileRes.java
@@ -1,9 +1,12 @@
 package com.umc.place.user.dto;
 
+import com.umc.place.story.entity.Story;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Data
 @RequiredArgsConstructor
@@ -12,11 +15,14 @@ public class GetProfileRes {
     private String nickname;
     private String comment;
     private int signupYear;
+    private Optional<Story> storyList;
 
-    public GetProfileRes(String userImg, String nickname, String comment, int signupYear) {
+
+    public GetProfileRes(String userImg, String nickname, String comment, int signupYear, Optional<Story> storyList) {
         this.userImg = userImg;
         this.nickname = nickname;
         this.comment = comment;
         this.signupYear = signupYear;
+        this.storyList = storyList;
     }
 }
