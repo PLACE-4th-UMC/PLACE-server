f0eac46e035ec3cd87de8530d69e6a60384c9229
ri-naa <rina288@ewhain.net>
2023-08-10 00:39:40 +0900

    #13 fix: user 레포지토리 수정

--- place/src/main/java/com/umc/place/user/repository/UserRepository.java
+++ place/src/main/java/com/umc/place/user/repository/UserRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByNickname(String nickname);
-    Optional<User> findByUserIdxAndStatus(Long userIdx, String status);
+    Optional<User> findByUserIdx(Long userIdx);
     User findByIdentifierAndProvider(String identifier, Provider provider);
     Optional<User> findByIdentifierAndStatus(String identifier, String status);
 
