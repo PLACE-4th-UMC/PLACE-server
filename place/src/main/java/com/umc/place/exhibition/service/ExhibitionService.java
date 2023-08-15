f5227e8ad4fadb4f1ce825ca602b8cfd5a851d8a
joonghyun <joongh@ewhain.net>
2023-08-15 13:37:18 +0900

    #5 fix: userIdx와 status로 user 조회하도록 수정

--- place/src/main/java/com/umc/place/exhibition/service/ExhibitionService.java
+++ place/src/main/java/com/umc/place/exhibition/service/ExhibitionService.java
@@ -119,8 +119,8 @@ public class ExhibitionService {
             Exhibition exhibition = exhibitionRepository.findById(exhibitionIdx).orElseThrow(() -> new BaseException(INVALID_EXHIBITION_IDX));
 
             List<GetExhibitionDetailRes.Story> storyList;
-            if (userRepository.existsById(userIdx)) {
-                User user = userRepository.findById(userIdx).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
+            if (userRepository.existsByUserIdxAndStatusEquals(userIdx, ACTIVE)) {
+                User user = userRepository.findByUserIdxAndStatusEquals(userIdx, ACTIVE).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
                 storyList = getStoryList(exhibition, user);
             } else storyList = getStoryListAnonymous(exhibition);
             //TODO: S3 설정 완료 후 이미지 조회 부분 수정
@@ -184,9 +184,9 @@ public class ExhibitionService {
     public void likeExhibition(Long exhibitionIdx, Long userIdx) throws BaseException {
         try {
             Exhibition exhibition = exhibitionRepository.findById(exhibitionIdx).orElseThrow(() -> new BaseException(INVALID_EXHIBITION_IDX));
-            // TODO: 비회원 예외 처리
-            User user = userRepository.findById(userIdx).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
+            User user = userRepository.findByUserIdxAndStatusEquals(userIdx, ACTIVE).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
             ExhibitionLike exhibitionLike = exhibitionLikeRepository.findByExhibitionAndUser(exhibition, user);
+
             if (exhibitionLike == null) { // 첫 좋아요
                 exhibitionLike = ExhibitionLike.builder()
                         .exhibition(exhibition)
