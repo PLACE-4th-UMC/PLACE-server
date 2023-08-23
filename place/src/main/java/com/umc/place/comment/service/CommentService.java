b4151cf7dc9700346bfda789ae7589fe654b715c
5jisoo <56earls@gmail.com>
2023-08-23 13:41:19 +0900

    #7 fix: set status INACTIVE

--- place/src/main/java/com/umc/place/comment/service/CommentService.java
+++ place/src/main/java/com/umc/place/comment/service/CommentService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.umc.place.common.BaseResponseStatus.*;
+import static com.umc.place.common.Constant.INACTIVE;
 
 @Service
 @RequiredArgsConstructor
@@ -55,7 +56,7 @@ public class CommentService {
                 throw new BaseException(NOT_OWNER);
             }
 
-            commentRepository.delete(comment);
+            comment.setStatus(INACTIVE);
             story.getComments().remove(comment); // 양방향 연관관계
 
             return null;
