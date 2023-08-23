577c5efd3f18e7cc0fda727acda36f6b75447aa3
Rina Kim <121746871+ri-naa@users.noreply.github.com>
2023-08-24 03:38:00 +0900

    Update start.sh

--- place/scripts/start.sh
+++ place/scripts/start.sh
@@ -10,7 +10,7 @@ START_LOG="$ROOT_PATH/start.log"
 NOW=$(date +%c)
 
 echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/place-1.0.0.jar $JAR
+cp $ROOT_PATH/build/libs/*.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
