e5d45b5b547a55a7a4833db1f0ea60ffba9e49ac
JoongHyun Kim <joongh@ewhain.net>
2023-08-24 02:41:10 +0900

    Update TestController.java

--- place/src/main/java/com/umc/place/common/TestController.java
+++ place/src/main/java/com/umc/place/common/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        return "Hello World!";
+        return "Hello, World!";
     }
 }
