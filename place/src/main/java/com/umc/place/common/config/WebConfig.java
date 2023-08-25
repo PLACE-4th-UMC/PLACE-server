584a309b9912f816250b437b31d2ac8c89eb2503
joonghyun <joongh@ewhain.net>
2023-08-25 23:00:04 +0900

    #44 fix: CORS 이슈 해결

---
+++ place/src/main/java/com/umc/place/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.umc.place.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://exhibition-place.site") // 프톤트 배포 후 수정
+                .allowedMethods("GET", "POST", "PATCH") // DELETE 허용 X
+                .allowCredentials(true)
+                .exposedHeaders("Authorization", "Set-Cookie"); // cookie 사용을 위한 HttpServlet 포함 여부
+    }
+}
